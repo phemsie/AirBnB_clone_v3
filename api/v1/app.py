e41b7ef9098c46cde9aa2f5d37c92b009c7de64a
phemsie <femi_oworu@yahoo.com>
2023-05-03 00:48:58 +0100

    AirBnB_clone_v3

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 """
-This module creates a Flask app for the RESTful API
+This module Flask app for the RESTful API
 """
 from flask import Flask, jsonify
 from api.v1.views import app_views
 from models import storage
 import os
+from flask_cors import CORS
+
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
+cors = CORS(app, resources={r"/api/*": {"origin": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown_app(exception):
@@ -22,12 +26,12 @@ def status():
     """Return the status of the API"""
     return jsonify({"status": "OK"})
 
-
 @app.errorhandler(404)
 def not_found(error):
     """ Handles 404 errors by returning a JSON-formatted 404 status code response.
     """
     return jsonify({'error': 'Not found'}), 404
+
 
 
 if __name__ == "__main__":
